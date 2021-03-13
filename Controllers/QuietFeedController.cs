e173401e6767c5547a7f39a351770718ba2519fa
Andrew Chapman <chapman@chaptek.net>
2021-03-13 17:03:31 -0500

    Runs locally with docker

--- Controllers/QuietFeedController.cs
+++ Controllers/QuietFeedController.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using QuietRssApi.Models;
+using QuietRssNuget;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace QuietRssApi.Controllers
 {
-    public class QuietFeedController
+    [Route("[controller]")]
+    [ApiController]
+    public class QuietFeedController : ControllerBase
     {
 
         private List<RssCompanyModel> _db;
@@ -16,8 +19,8 @@ namespace QuietRssApi.Controllers
             _db = new List<RssCompanyModel>();
         }
 
-        [HttpPost]
         //[Authorize]
+        [HttpPost]
         public IActionResult Post([FromBody] string company, string rss_uri)
         {
             if (!Uri.IsWellFormedUriString(rss_uri, UriKind.RelativeOrAbsolute))
@@ -32,18 +35,18 @@ namespace QuietRssApi.Controllers
             };
 
             _db.Add(entry);
-            return new CreatedResult("quietfeed/" + entry.Id.ToString(), entry);
+            return new CreatedResult("/" + entry.Id.ToString(), entry);
 
         }
 
 
-        [HttpDelete("{id}")]
         //[Authorize]
+        [HttpDelete("{id}")]
         public IActionResult Delete(string id)
         {
             try
             {
-                if( Guid.TryParse(id, out Guid gId))
+                if (Guid.TryParse(id, out Guid gId))
                 {
                     _db.Remove(_db.Where(r => r.Id == gId).Single());
                     return new OkResult();
@@ -57,16 +60,11 @@ namespace QuietRssApi.Controllers
             }
         }
 
-
-        [HttpGet]
-        public List<RssCompanyModel> Get() => _db;
-
-
         ///summary: primary method - days is consecutive days in which an RSS feed hasn't beeen updated.<!-- -->
         [HttpGet]
-        public ActionResult Get(int days = 5)
+        public HashSet<RssSummary> Get(double days = 5)
         {
-            return null;
+            return RssRetriever.GetQuietFeeds(_db.Select(x => x.Data).ToList(), days);
         }
     }
 }
--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
 #See https://aka.ms/containerfastmode to understand how Visual Studio uses this Dockerfile to build your images for faster debugging.
 
-FROM mcr.microsoft.com/dotnet/aspnet:5.0 AS base
-WORKDIR /app
-EXPOSE 80
-EXPOSE 443
-
 FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build
 WORKDIR /src
 COPY . .
@@ -14,7 +9,9 @@ RUN dotnet build "QuietRssApi.csproj" -c Release -o /app/build
 FROM build AS publish
 RUN dotnet publish "QuietRssApi.csproj" -c Release -o /app/publish
 
-FROM base AS final
+FROM mcr.microsoft.com/dotnet/aspnet:5.0 AS final
 WORKDIR /app
 COPY --from=publish /app/publish .
+EXPOSE 5000
+EXPOSE 5001
 ENTRYPOINT ["dotnet", "QuietRssApi.dll"]
