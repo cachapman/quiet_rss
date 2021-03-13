1745317e8a02420e31d3cb35df7b8f34ccb37d21
Andrew Chapman <chapman@chaptek.net>
2021-03-13 16:01:02 -0500

    Consolidated and moved controllers into each other (this app is for testing the nuget package where the logic lives) Added a new model to associate an id for gets and deletes

--- Controllers/QuietFeedController.cs
+++ Controllers/QuietFeedController.cs
@@ -1,11 +1,71 @@
 using Microsoft.AspNetCore.Mvc;
+using QuietRssApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace RssQuiet.Controllers
+namespace QuietRssApi.Controllers
 {
     public class QuietFeedController
     {
+
+        private List<RssCompanyModel> _db;
+
+        public QuietFeedController()
+        {
+            _db = new List<RssCompanyModel>();
+        }
+
+        [HttpPost]
+        //[Authorize]
+        public IActionResult Post([FromBody] string company, string rss_uri)
+        {
+            if (!Uri.IsWellFormedUriString(rss_uri, UriKind.RelativeOrAbsolute))
+            {
+                return new BadRequestResult();
+            }
+
+            var entry = new RssCompanyModel
+            {
+                Id = Guid.NewGuid(),
+                Data = new Tuple<string, string>(company, rss_uri)
+            };
+
+            _db.Add(entry);
+            return new CreatedResult("quietfeed/" + entry.Id.ToString(), entry);
+
+        }
+
+
+        [HttpDelete("{id}")]
+        //[Authorize]
+        public IActionResult Delete(string id)
+        {
+            try
+            {
+                if( Guid.TryParse(id, out Guid gId))
+                {
+                    _db.Remove(_db.Where(r => r.Id == gId).Single());
+                    return new OkResult();
+                }
+                return new BadRequestResult();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception on writing to the db - " + e.Message);
+                return new BadRequestResult();
+            }
+        }
+
+
+        [HttpGet]
+        public List<RssCompanyModel> Get() => _db;
+
+
         ///summary: primary method - days is consecutive days in which an RSS feed hasn't beeen updated.<!-- -->
-          public ActionResult Get(int? days = 5, int? limit = 10, int? per_page = 5, int page = 1 ) {
+        [HttpGet]
+        public ActionResult Get(int days = 5)
+        {
             return null;
         }
     }
---
+++ RssCompanyModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuietRssApi.Models
+{
+    public class RssCompanyModel
+    {
+        public Guid Id { get; set; }
+        public Tuple<string, string> Data { get; set; }
+    }
+}
